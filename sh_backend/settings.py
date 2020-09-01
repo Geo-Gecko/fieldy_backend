fe15435ed9f76341a2dac7b51c4c0eb56e2760c2
RonKbS <stephen.bamwite@gmail.com>
2020-09-01 18:21:33 +0300

    restrict connections to localhost and hosted platform

--- sh_backend/settings.py
+++ sh_backend/settings.py
@@ -79,8 +79,7 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'sh_backend.wsgi.application'
 
-# CORS_ALLOWED_ORIGINS = ['http://127.0.0.1:3000']
-CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOWED_ORIGINS = [os.environ.get('SH_FRONTEND', 'http://127.0.0.1:3000')]
 
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
