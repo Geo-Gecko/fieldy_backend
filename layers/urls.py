3f0d6aa5e0dc9e5c7f766ed695bfa435c6d0bcb9
RonKbS <stephen.bamwite@gmail.com>
2020-09-09 17:02:02 +0300

    add features to delete and edit layers

--- layers/urls.py
+++ layers/urls.py
@@ -1,12 +1,13 @@
 from django.urls import include, path
 from rest_framework import routers
 from layers.views import (
-    ListCreatePointLayer, ListCreatePolygonLayer
+    ListCreatePointLayer, ListCreatePolygonLayer, RetrieveUpdateDestroyPolygonLayer
 )
 
 urlpatterns = [
     path('listcreatepointlayer/', ListCreatePointLayer.as_view()),
     path('listcreatepolygonlayer/', ListCreatePolygonLayer.as_view()),
+    path('<str:field_id>/', RetrieveUpdateDestroyPolygonLayer.as_view())
 ]
 
 
--- layers/views.py
+++ layers/views.py
@@ -1,6 +1,6 @@
 import os
 
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
 from rest_framework import (
     exceptions,
     generics,
@@ -19,9 +19,9 @@ from .models import PolygonLayer, PointLayer
 from .serializers import PointLayerSerializer, PolygonLayerSerializer
 
 def verify_auth_token(request):
-    token = request.headers["Authorization"]
-    token = token.split(" ")[1]
+    token = request.headers.get("Authorization")
     if token:
+        token = token.split(" ")[1]
         try:
             user_ = jwt.decode(token, os.environ.get("SECRET_KEY", ""), algorithms="HS256")
             return request.data
@@ -68,3 +68,40 @@ class ListCreatePolygonLayer(ListCreateLayers):
 
     serializer_class = PolygonLayerSerializer
     queryset = PolygonLayer.objects.all()
+
+class RetrieveUpdateDestroyPolygonLayer(
+    generics.RetrieveUpdateDestroyAPIView
+):
+
+    queryset = PolygonLayer.objects.all()
+    serializer_class = PolygonLayerSerializer
+    permission_classes = (AllowAny,)
+
+    def put(self, request, field_id):
+        serializer_data = verify_auth_token(request)
+        if not serializer_data:
+            return Response(
+                {"Error": "Unauthorized request"},
+                status=status.HTTP_403_FORBIDDEN
+            )
+        layer_ = get_object_or_404(PolygonLayer, field_id=field_id)
+        serializer = self.serializer_class(layer_, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def delete(self, request, field_id):
+        user_data = verify_auth_token(request)
+        if user_data != {}:
+            return Response(
+                {"Error": "Unauthorized request"},
+                status=status.HTTP_403_FORBIDDEN
+            )
+        layer_ = get_object_or_404(PolygonLayer, field_id=field_id)
+        layer_.delete()
+
+        return Response(
+            {"message": "Layer has been deleted"},
+            status=status.HTTP_204_NO_CONTENT
+        )
