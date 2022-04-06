502cf9777d58999db67d332806c349ebc91901ea
Desire Kaczynski Rukud'de <rukuddez@gmail.com>
2022-04-06 11:35:51 +0300

    changes to reflect the processing for oaf

--- layers/views/one_acre_views.py
+++ layers/views/one_acre_views.py
@@ -1,5 +1,6 @@
 import os
 from datetime import datetime
+from dateutil.relativedelta import relativedelta
 
 from django.db.models import Count
 from django.contrib.postgres.fields.jsonb import KeyTextTransform
@@ -45,9 +46,45 @@ def get_last_visit_summaries(request, month_=1):
         ).values("LastVisit").annotate(fieldCount=Count("LastVisit"))
         results_ = list(filter(
             lambda row_: (
-                datetime.now() - datetime.strptime(row_["LastVisit"], '%Y-%M-%d')
+                datetime.now() - datetime.strptime(row_["LastVisit"], '%Y-%m-%d')
             ).days // 31 <= month_, results_
         ))
+        return Response(results_, status=status.HTTP_200_OK)
+    except PolygonJsonLayer.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+@api_view(['GET'])
+def get_status_summaries(request, status_="Currently"):
+    """Return last visits less or equal to a month
+
+    Returns:
+        [200]: List date-visited-field-count dictionaries
+    """
+    user_data, user = verify_auth_token(request)
+    if user_data != {}:
+        return Response(
+            {"Error": "Unauthorized request"},
+            status=status.HTTP_403_FORBIDDEN
+        )
+
+    if user["memberOf"] != "":
+        user["uid"] = user["memberOf"]
+    
+    if user["uid"] != "623731215344c1000aae2459":
+        return Response(
+            {"Error": "This data is not available for this account"},
+            status=status.HTTP_403_FORBIDDEN
+        )
+    # if status_ > 12:
+    #     return Response(
+    #         {"Error": "Max and min available months are 0 and 12"},
+    #         status=status.HTTP_403_FORBIDDEN
+    #     )
+
+    try:
+        results_ = PolygonJsonLayer.objects.filter(user_id=user["uid"]).annotate(
+            OAFStatus=KeyTextTransform("OAFStatus", "properties")
+        ).values("OAFStatus").annotate(fieldCount=Count("OAFStatus"))
         return Response(results_, status=status.HTTP_200_OK)
     except PolygonJsonLayer.DoesNotExist:
         return Response(status=status.HTTP_404_NOT_FOUND)
